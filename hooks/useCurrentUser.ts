cf61e1700a7e56538cbf4b785d7c208a6d773594
Frani BIZI <bizifrani@icloud.com>
2023-07-04 19:33:01 +0200

    🎨  Improve structure

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,8 +1,17 @@
-import useSwr from "swr";
+import useSwr, { KeyedMutator } from "swr";
 
 import fetcher from "@/pages/fetcher";
+import { User } from "@/shared/types/user";
+import { NextError } from "@/shared/types/common";
 
-const useCurrentUser = () => {
+interface UseCurrentUser {
+  data: User;
+  error: NextError;
+  isLoading: boolean;
+  mutate: KeyedMutator<User>;
+}
+
+const useCurrentUser = (): UseCurrentUser => {
   const { data, error, isLoading, mutate } = useSwr("/api/current", fetcher);
   return {
     data,
