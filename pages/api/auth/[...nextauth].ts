1593ebc65f38ec2693207f1177d82d327bfd27f5
Frani BIZI <bizifrani@icloud.com>
2023-07-18 03:19:07 +0200

    👷  update nextauth file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,12 @@
-import NextAuth from "next-auth/next";
-import Credentials from "next-auth/providers/credentials";
-import prismadb from "../../../lib/prismadb";
-import { compare } from "bcrypt";
-
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { compare } from "bcrypt";
+import prismadb from "@/lib/prismadb";
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
@@ -69,4 +67,6 @@ export default NextAuth({
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+export default NextAuth(authOptions);
